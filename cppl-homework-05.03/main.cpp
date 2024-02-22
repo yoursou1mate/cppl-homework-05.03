63154c68f9afb0fd767b2a939093eb672a20f1cc
a1ex.solovyov <a1ex.solovyov10@gmail.com>
2024-02-22 11:20:40 +0300

    05.03_Second commit

--- cppl-homework-05.03/main.cpp
+++ cppl-homework-05.03/main.cpp
@@ -7,63 +7,54 @@
 
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 class SimpleFunctor {
 private:
-    unsigned size = 0;
-    int* arr = new int[size];
+    std::vector<int> arr;
     unsigned m_counter = 0;
 public:
-    SimpleFunctor(unsigned size) : size(size) {}
+    SimpleFunctor() : arr() {}
 
-    int* getArr() {
-        return arr;
-    }
-
-    void operator()()
+    int* getArr()
     {
-        std::for_each(arr, arr + size,[this](int& i)
-        {
-          [&](int& i)
-          {
-            return 0;
-          }(i);
-              int sum = 0;
-              sum += i;
-              if(i%3 == 0)
-              {
-                  ++m_counter;
-              }
-        });
-    };
-
-    unsigned get_count()
+        return arr.data();
+    }
+    unsigned get_count() const
     {
         return m_counter;
     }
+
+    void operator()(int& value)
+    {
+    arr.push_back(value);
+      if(value % 3 == 0)
+      {
+        m_counter++;
+      }
+    }
+
+    int get_sum()
+    {
+    int sum = 0;
+    for (int i = 0; i < arr.size(); i++)
+      {
+        sum += arr[i];
+      }
+      return sum;
+    }
+
 };
+       
 
 
 int main(int argc, const char * argv[]) {
 
-    SimpleFunctor get_sum(5);
-    std::cout << "[IN]: ";
-    for (int i = 0; i < 5; i++)
-    {
-        int input;
-        std::cin >> input;
-        get_sum.getArr()[i] = input;
-    }
-    std::cout << "[OUT]: ";
-    get_sum();
-    for (int i = 0; i < 5; i++)
-      {
-        std::cout << get_sum.getArr()[i] << " ";
-      }
-    std::cout << std::endl;
-    std::cout << "[OUT]: " << get_sum.get_count();
-    std::cout << std::endl;
 
+    std::vector<int> numbers = { 4, 1, 3, 6, 25, 54 };
+    SimpleFunctor counter = std::for_each(numbers.begin(), numbers.end(), SimpleFunctor());
+    std::cout << "[OUT]: get_sum() = " << counter.get_sum() << std::endl;
+    std::cout << "[OUT]: get_count() = " << counter.get_count() << std::endl;
 
     return 0;
 }
